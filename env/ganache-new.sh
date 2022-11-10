15d180a6b312e19b4e153f3b4eb79ea866a1d480
John Whitton <john@johnwhitton.com>
2022-11-09 17:28:33 -0800

    Use 2 second block time for ganache

--- env/ganache-new.sh
+++ env/ganache-new.sh
@@ -4,4 +4,5 @@
 # ganache -b 2 --server.ws --database.dbPath "./db" -m "test test test test test test test test test test test junk"
 
 rm -rf db
-ganache --server.ws --database.dbPath "./db" -m "test test test test test test test test test test test junk"
+# ganache --server.ws --database.dbPath "./db" -m "test test test test test test test test test test test junk"
+ganache -b 2 --server.ws --database.dbPath "./db" -m "test test test test test test test test test test test junk"
