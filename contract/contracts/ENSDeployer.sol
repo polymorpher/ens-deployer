ac3e5492c54b2eb3c893271142420b81875adde4
polymorpher <aaron@qokka.ai>
2022-12-20 21:45:58 -0800

    prettier

--- contract/contracts/ENSDeployer.sol
+++ contract/contracts/ENSDeployer.sol
@@ -17,11 +17,7 @@ library ENSUtils {
     bytes32 public constant REVERSE_REGISTRAR_LABEL = keccak256("reverse");
     bytes32 public constant ADDR_LABEL = keccak256("addr");
 
-    function namehash(bytes32 node, bytes32 label)
-        public
-        pure
-        returns (bytes32)
-    {
+    function namehash(bytes32 node, bytes32 label) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(node, label));
     }
 
@@ -31,15 +27,9 @@ library ENSUtils {
 }
 
 library ENSRegistryDeployer {
-    function deployRegistry(string memory tld)
-        public
-        returns (
-            ENSRegistry ens,
-            FIFSRegistrar fifsRegistrar,
-            ReverseRegistrar reverseRegistrar,
-            BaseRegistrarImplementation baseRegistrar
-        )
-    {
+    function deployRegistry(
+        string memory tld
+    ) public returns (ENSRegistry ens, FIFSRegistrar fifsRegistrar, ReverseRegistrar reverseRegistrar, BaseRegistrarImplementation baseRegistrar) {
         bytes32 tld_label = keccak256(bytes(tld));
         ens = new ENSRegistry();
 
@@ -52,36 +42,17 @@ library ENSRegistryDeployer {
         reverseRegistrar = new ReverseRegistrar(ens);
 
         // Set up the reverse registrar
-        ens.setSubnodeOwner(
-            bytes32(0),
-            ENSUtils.REVERSE_REGISTRAR_LABEL,
-            address(this)
-        );
-        ens.setSubnodeOwner(
-            ENSUtils.namehash(bytes32(0), ENSUtils.REVERSE_REGISTRAR_LABEL),
-            ENSUtils.ADDR_LABEL,
-            address(reverseRegistrar)
-        );
+        ens.setSubnodeOwner(bytes32(0), ENSUtils.REVERSE_REGISTRAR_LABEL, address(this));
+        ens.setSubnodeOwner(ENSUtils.namehash(bytes32(0), ENSUtils.REVERSE_REGISTRAR_LABEL), ENSUtils.ADDR_LABEL, address(reverseRegistrar));
 
-        baseRegistrar = new BaseRegistrarImplementation(
-            ens,
-            ENSUtils.namehash(tld_label)
-        );
+        baseRegistrar = new BaseRegistrarImplementation(ens, ENSUtils.namehash(tld_label));
         ens.setSubnodeOwner(bytes32(0), tld_label, address(this));
     }
 }
 
 library ENSNFTDeployer {
-    function deployNFTServices(
-        ENS ens,
-        BaseRegistrarImplementation baseRegistrar
-    )
-        public
-        returns (IMetadataService metadataService, NameWrapper nameWrapper)
-    {
-        metadataService = IMetadataService(
-            address(new StaticMetadataService("https://modulo.so/ens/metadata"))
-        );
+    function deployNFTServices(ENS ens, BaseRegistrarImplementation baseRegistrar) public returns (IMetadataService metadataService, NameWrapper nameWrapper) {
+        metadataService = IMetadataService(address(new StaticMetadataService("https://modulo.so/ens/metadata")));
         nameWrapper = new NameWrapper(ens, baseRegistrar, metadataService);
     }
 }
@@ -114,12 +85,7 @@ library ENSPublicResolverDeployer {
         RegistrarController registrarController,
         ReverseRegistrar reverseRegistrar
     ) public returns (PublicResolver publicResolver) {
-        publicResolver = new PublicResolver(
-            ens,
-            INameWrapperForPublicResolver(address(nameWrapper)),
-            address(registrarController),
-            address(reverseRegistrar)
-        );
+        publicResolver = new PublicResolver(ens, INameWrapperForPublicResolver(address(nameWrapper)), address(registrarController), address(reverseRegistrar));
         bytes32 resolverNode = ENSUtils.namehash(ENSUtils.RESOLVER_LABEL);
 
         //        note that in newer ens-contract deployment script, resolver address is set to `resolver.<tld>`, instead of just `resolver`. I am not sure if that has any significance, if it does we should update the lines below to be consistent with that
@@ -152,12 +118,7 @@ contract ENSDeployer is Ownable {
     function deployResolver(string memory tld) public onlyOwner {
         bytes32 tld_label = keccak256(bytes(tld));
         bytes32 tld_node = ENSUtils.namehash(tld_label);
-        publicResolver = ENSPublicResolverDeployer.deployResolver(
-            ens,
-            nameWrapper,
-            registrarController,
-            reverseRegistrar
-        );
+        publicResolver = ENSPublicResolverDeployer.deployResolver(ens, nameWrapper, registrarController, reverseRegistrar);
         reverseRegistrar.setDefaultResolver(address(publicResolver));
 
         // get from scripts/computeInterfaceId.ts
@@ -167,40 +128,20 @@ contract ENSDeployer is Ownable {
     }
 
     function deployUtils() public onlyOwner {
-        universalResolver = new UniversalResolver(
-            address(ens),
-            new string[](0)
-        );
+        universalResolver = new UniversalResolver(address(ens), new string[](0));
     }
 
     function deployRegistrar(string memory tld) public onlyOwner {
-        (
-            ens,
-            fifsRegistrar,
-            reverseRegistrar,
-            baseRegistrar
-        ) = ENSRegistryDeployer.deployRegistry(tld);
+        (ens, fifsRegistrar, reverseRegistrar, baseRegistrar) = ENSRegistryDeployer.deployRegistry(tld);
     }
 
     function deployNFTServices() public onlyOwner {
-        (metadataService, nameWrapper) = ENSNFTDeployer.deployNFTServices(
-            ens,
-            baseRegistrar
-        );
+        (metadataService, nameWrapper) = ENSNFTDeployer.deployNFTServices(ens, baseRegistrar);
         baseRegistrar.addController(address(nameWrapper));
     }
 
-    function deployController(string memory tld, IPriceOracle priceOracle)
-        public
-        onlyOwner
-    {
-        registrarController = ENSControllerDeployer.deployController(
-            tld,
-            priceOracle,
-            nameWrapper,
-            baseRegistrar,
-            reverseRegistrar
-        );
+    function deployController(string memory tld, IPriceOracle priceOracle) public onlyOwner {
+        registrarController = ENSControllerDeployer.deployController(tld, priceOracle, nameWrapper, baseRegistrar, reverseRegistrar);
         nameWrapper.setController(address(registrarController), true);
         reverseRegistrar.setController(address(registrarController), true);
     }
