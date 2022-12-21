c9211f94d26006f7191f9d316c63c0378208bc44
polymorpher <aaron@qokka.ai>
2022-12-20 22:11:16 -0800

    oracle price; cleanup

--- contract/deploy/00_deploy_registry.ts
+++
@@ -1,8 +0,0 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { ethers } from 'hardhat'
-
-const f = async function (hre: HardhatRuntimeEnvironment) {
-    // TODO
-}
-f.tags = ['Registry', 'Full']
-export default f
