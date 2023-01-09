62e7140291ec1af51582c7dad9ef07a6ad101139
John Whitton <john@johnwhitton.com>
2023-01-09 01:13:37 -0800

    minor cleanup of unused code in dns.ts

--- contract/test/utilities/dns.ts
+++ contract/test/utilities/dns.ts
@@ -5,28 +5,6 @@ import { Constants, utils } from '../utilities'
 // import { namehash } from 'eth-ens-namehash'
 const namehash = require('eth-ens-namehash')
 
-export const records = [
-  {
-    name: 'www.test.country.',
-    type: 'A',
-    ttl: 3600,
-    data: '127.0.0.1' // fx 127.0.0.1
-  },
-  {
-    created: '2019-09-18T16:32:16.510368Z',
-    domain: 'test.country',
-    subname: 'www',
-    name: 'www.test.country.',
-    type: 'A',
-    records: [
-      '127.0.0.1',
-      '127.0.0.2'
-    ],
-    ttl: 3600,
-    touched: '2020-04-06T09:24:09.987436Z'
-  }
-]
-
 export function hexEncodeName (name) {
   return '0x' + packet.name.encode(name).toString('hex')
 }
@@ -79,14 +57,6 @@ export function dnsName (name) {
         )
   )
 }
-
-// export function encodeRecords (records) {
-//   const buf = encode(records[0])
-//   console.log(`records: ${JSON.stringify(records)}`)
-//   console.log(`buf: ${JSON.stringify(buf)}`)
-//   console.log(`bufDecode: ${JSON.stringify(decode(buf))}`)
-//   return records
-// }
 
 export function encodeARecord (recName, recAddress) {
   // a.country. 3600 IN A 1.2.3.4
