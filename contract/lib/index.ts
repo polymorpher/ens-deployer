e41635b976bfbb8cbdbeb8124502566e366b71c1
polymorpher <aaron@qokka.ai>
2023-02-11 22:38:11 -0800

    cleanup

--- contract/lib/index.ts
+++ contract/lib/index.ts
@@ -1,10 +1,7 @@
-// export const Constants = require('./constants')
 import { Constants } from './constants'
 import * as dns from './dns'
-import * as utils from './utils'
 
 export {
   Constants,
-  dns,
-  utils
+  dns
 }
