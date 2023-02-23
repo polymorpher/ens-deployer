4da285320ecfc0f6157bff976dae5d39fb200bca
polymorpher <aaron@qokka.ai>
2023-02-23 00:37:13 -0800

    fix a bug in oracle deployment

--- contract/contracts/OracleDeployer.sol
+++ contract/contracts/OracleDeployer.sol
@@ -9,7 +9,12 @@ contract OracleDeployer is Ownable {
     IPriceOracle public oracle;
 
     constructor(uint256 _assetPriceNanoUSD, uint256 _baseUnitPrice, uint8[] memory _lengths, uint256[] memory _premiumUnitPrices) {
-        usdOracle = USDOracleInterface(address(new SimpleAssetPriceOracle(_assetPriceNanoUSD)));
+        SimpleAssetPriceOracle u = new SimpleAssetPriceOracle(_assetPriceNanoUSD);
+        u.grantRole(u.DEFAULT_ADMIN_ROLE(), msg.sender);
+        u.grantRole(u.ROLE_FEEDER(), msg.sender);
+        u.renounceRole(u.ROLE_FEEDER(), address(this));
+        u.renounceRole(u.DEFAULT_ADMIN_ROLE(), address(this));
+        usdOracle = USDOracleInterface(address(u));
         oracle = new LengthBasedPriceOracle(usdOracle, _baseUnitPrice, _lengths, _premiumUnitPrices);
     }
 }
